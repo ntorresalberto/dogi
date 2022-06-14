e7b2d310fe013f7e646d5ba9aab581b472bd1550
Nicolas Torres <nicolastorresalberto@gmail.com>
2022-06-14 14:48:43 +0200

    comment uneeded deps in debug command and update mod file

--- cmd/debug.go
+++ cmd/debug.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
-	"context"
+	// "context"
 	"syscall"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
+	// "github.com/docker/docker/api/types"
+	// "github.com/docker/docker/client"
 )
 
 var debugCmd = &cobra.Command{
@@ -14,26 +14,27 @@ var debugCmd = &cobra.Command{
 	Short: "a command to debug dogi",
 	Long:  `This command provides an interface to test and see internal dogi information.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
-		cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-		check(err)
+		// ctx := context.Background()
+		// cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+		// check(err)
 
-		// images
-		imgs, err := cli.ImageList(ctx, types.ImageListOptions{})
-		check(err)
-		logger.Printf("docker images:")
-		for _, img := range imgs {
-			logger.Printf("%s: %d", img.RepoTags, img.Containers)
-		}
+		// // images
+		// imgs, err := cli.ImageList(ctx, types.ImageListOptions{})
+		// check(err)
+		// logger.Printf("docker images:")
+		// for _, img := range imgs {
+		// 	logger.Printf("%s: %d", img.RepoTags, img.Containers)
+		// }
 
-		// containers
-		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-		check(err)
-		logger.Printf("docker containers:")
-		for _, container := range containers {
-			logger.Printf("%s %s\n", container.ID[:10], container.Image)
-		}
+		// // containers
+		// containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+		// check(err)
+		// logger.Printf("docker containers:")
+		// for _, container := range containers {
+		// 	logger.Printf("%s %s\n", container.ID[:10], container.Image)
+		// }
 
+		logger.Println("debug command not implemented!")
 		syscall.Exit(0)
 	},
 }
